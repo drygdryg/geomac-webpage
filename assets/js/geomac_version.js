a364321dd3551ddf7407f6da54731bef2f78d94c
Victor Golovanenko <drygdryg2014@yandex.com>
2021-03-02 07:39:36 +0300

    The first version of the web page

---
+++ assets/js/geomac_version.js
@@ -0,0 +1,4 @@
+window.onload  = function() {
+    const geomac_release = "0.1";
+    document.getElementById("geomac_release").innerHTML = geomac_release;
+}
---
+++ assets/js/switch_to_russian.js
@@ -0,0 +1,4 @@
+var language = (navigator.language || navigator.userLanguage).substring(0, 2);
+if (language == "ru" || language == "uk" || language == "by") {
+    window.location.replace("./index_russian.html");
+}
